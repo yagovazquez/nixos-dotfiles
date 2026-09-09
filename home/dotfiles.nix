fb28378e85e30555841dca3ad56f18dfc7a5170e
yago <vazquezsanchezyago@gmail.com>
2026-09-09 19:37:20 +0200

    hyprland.conf removed

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -6,7 +6,7 @@
   home.file.".config/qutebrowser/config.py".source = ./qutebrowser/config.py;
   home.file.".config/yazi/yazi.toml".source = ./yazi/yazi.toml;
   home.file.".config/hypr/hyprpaper.conf".source = ./hypr/hyprpaper.conf;
-  home.file.".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
+  #home.file.".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
   home.file.".config/hypr/hyprland.lua".source = ./hypr/hyprland.lua;
   #xdg.configFile."hypr".source = ./hypr;
   xdg.configFile."niri".source = ./niri;
